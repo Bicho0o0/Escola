af3d3b6e3247d0c7bc2f4249eebe462e0152f6ca
Bicho0o0 <jfabrciovs007@gmail.com>
2026-09-17 21:46:18 -0700

    Se agrego boton de guardado

--- registro.py
+++ registro.py
@@ -146,6 +146,16 @@ entrada_celular.grid(
 )
 
 
+boton_guardar = tk.Button(
+    ventana,
+    text="GUARDAR REGISTRO",
+    command=guardar_registro,
+    width=30,
+    height=2
+)
+
+boton_guardar.pack(pady=20)
+
 #jeje
 
 # ==========================================
